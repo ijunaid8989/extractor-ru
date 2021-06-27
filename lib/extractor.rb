756758c470bbd32959d9af368d88bd451b45dd2e
Junaid Farooq <junaid@evercam.io>
2021-06-27 23:45:42 +0500

    Initial Commit.

---
+++ lib/extractor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "extractor/version"
+
+module Extractor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/extractor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Extractor
+  VERSION = "0.1.0"
+end
